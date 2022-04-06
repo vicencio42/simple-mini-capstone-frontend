8a4f2b3159dec35dad2a466a10a24d81b2e155bf
Vicencio42 <vicencio42@gmail.com>
2022-04-05 21:07:23 -0500

    added delete product method, edited create

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,20 +3,53 @@
     <h1 v-bind:class="message">{{ message }} count: {{ products.length }}</h1>
     <h2 class="greeting">{{ greeting }}</h2>
     Name
-    <input type="text" v-model="productName" />
+    <input type="text" v-model="newProduct.name" />
     Price
-    <input type="text" v-model="productPrice" />
+    <input type="text" v-model="newProduct.price" />
     Description
-    <input type="text" v-model="productDescription" />
+    <input type="text" v-model="newProduct.description" />
     Image URL
-    <input type="text" v-model="productImgUrl" />
+    <input type="text" v-model="newProduct.image_url" />
     <button v-on:click="createProduct">Create Stuff</button>
     <div v-for="product in products" v-bind:key="product.id">
       <h3>{{ product.name }}</h3>
       <p class="product">{{ product.price }}</p>
-      <img v-bind:src="product.image_url" v-bind:alt="product.name" />
+      <img v-bind:src="product.image_url" v-bind:alt="product.name" style="max-height: 200px" />
+      <div>
+        <button v-on:click="showProduct(product)">Product Info</button>
+      </div>
     </div>
   </div>
+  <dialog id="product-info">
+    <form method="dialog">
+      <h1>Product Info</h1>
+      <p>Name: {{ currentProduct.name }}</p>
+      <p>Price: {{ currentProduct.price }}</p>
+      <p>Description: {{ currentProduct.description }}</p>
+      <p>Image URL: {{ currentProduct.image_url }}</p>
+      <h1>Edit Product</h1>
+      <p>
+        Name:
+        <input v-model="currentProduct.name" type="text" />
+      </p>
+      <p>
+        Price:
+        <input v-model="currentProduct.price" type="text" />
+      </p>
+      <p>
+        Description:
+        <input v-model="currentProduct.description" type="text" />
+      </p>
+      <p>
+        Image URL:
+        <input v-model="currentProduct.image_url" type="text" />
+      </p>
+      <p></p>
+      <button v-on:click="updateProduct()">Update</button>
+      <button v-on:click="deleteProduct(currentProduct)">Delete</button>
+      <button>Close</button>
+    </form>
+  </dialog>
 </template>
 
 <script>
@@ -28,10 +61,8 @@ export default {
       message: "Welcome to Vue.js By Vin!",
       greeting: "Let's write some vue code!",
       products: [],
-      productName: "",
-      productDescription: "",
-      productPrice: "",
-      productImgUrl: "",
+      newProduct: {},
+      currentProduct: {},
     };
   },
   created: function () {
@@ -45,20 +76,31 @@ export default {
       });
     },
     createProduct() {
-      var params = {
-        name: this.productName,
-        price: this.productPrice,
-        description: this.productDescription,
-        image_url: this.productImgUrl,
-      };
-      axios
-        .post("http://localhost:3000/products", params)
-        .then((response) => {
-          this.products.push(response.data);
-        })
-        .catch((error) => {
-          console.log(error.response);
-        });
+      axios.post("http://localhost:3000/products", this.newProduct).then((response) => {
+        this.products.push(response.data);
+      });
+    },
+    showProduct(product) {
+      axios.get(`http://localhost:3000/products/${product.id}`).then((response) => {
+        this.currentProduct = response.data;
+        this.editProduct = response.data;
+        document.querySelector("#product-info").showModal();
+      });
+    },
+    updateProduct() {
+      axios.patch(`http://localhost:3000/products/${this.currentProduct.id}`, this.currentProduct).then((response) => {
+        console.log("Success!", response);
+        var index = this.products.indexOf(this.currentProduct);
+        this.products.splice(index, 1);
+        this.products.push(response.data);
+      });
+    },
+    deleteProduct(product) {
+      axios.delete(`http://localhost:3000/products/${product.id}`).then((response) => {
+        console.log(response);
+        var index = this.products.indexOf(product);
+        this.products.splice(index, 1);
+      });
     },
   },
 };
